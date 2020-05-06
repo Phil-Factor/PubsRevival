2876fd8acc08685db905eca8c647c01cc2cbbaff
Phil Factor <Phil.Factor@simple-talk.com>
2020-05-06 17:53:16 +0100

    initial build scripts from  existing Pubs database
    Initial

---
+++ Build/Tables/dbo.titles.sql
@@ -0,0 +1,20 @@
+CREATE TABLE [dbo].[titles]
+(
+[title_id] [dbo].[tid] NOT NULL,
+[title] [varchar] (80) COLLATE Latin1_General_CI_AS NOT NULL,
+[type] [char] (12) COLLATE Latin1_General_CI_AS NOT NULL DEFAULT ('UNDECIDED'),
+[pub_id] [char] (4) COLLATE Latin1_General_CI_AS NULL,
+[price] [money] NULL,
+[advance] [money] NULL,
+[royalty] [int] NULL,
+[ytd_sales] [int] NULL,
+[notes] [varchar] (200) COLLATE Latin1_General_CI_AS NULL,
+[pubdate] [datetime] NOT NULL DEFAULT (getdate())
+)
+GO
+ALTER TABLE [dbo].[titles] ADD CONSTRAINT [UPKCL_titleidind] PRIMARY KEY CLUSTERED  ([title_id])
+GO
+CREATE NONCLUSTERED INDEX [titleind] ON [dbo].[titles] ([title])
+GO
+ALTER TABLE [dbo].[titles] ADD FOREIGN KEY ([pub_id]) REFERENCES [dbo].[publishers] ([pub_id])
+GO
